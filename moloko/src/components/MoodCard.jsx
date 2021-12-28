6af602af08789a002378ff43aad787d649ce53ea
nysteo <teonys@nyu.edu>
2021-12-28 05:30:32 -0500

    ADD Dashboard

--- moloko/src/components/MoodCard.jsx
+++ moloko/src/components/MoodCard.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, {memo} from 'react';
 
-const MoodCard = ({name, author, vibes, colors}) => {
-  return (
-    <div className={`bg-slate-800 w-full rounded-sm border border-slate-700 p-4 shadow-${colors[1]}/40 shadow-md h-40`}>
-      <h2 className={`text-2xl font-bold text-transparent bg-gradient-to-r from-${colors[0]} to-${colors[1]} bg-clip-text`}>{name}</h2>
-      <div className='flex items-center space-x-4'>
-        <p>Vibes: </p>
-        {vibes && vibes.map((vibe, index) => (
-          <div
-            key={`${vibe}-${index}`}
-            className={`p-2 rounded-full bg-gradient-to-r from-${vibe.colors[0]} to-${vibe.colors[1]}`}
-          >
-            {vibe.name}
+const MoodCard = ({create = false, name,  vibes, colors, likes}) =>{
+
+  if (!create) {
+    return (
+      <div className={`bg-slate-800 w-full rounded-sm border border-slate-700 p-4 shadow-indigo-500/10 shadow-lg space-y-4 hover:shadow-indigo-400/20 cursor-pointer hover:border-slate-600`}>
+        <h2 className={`text-3xl font-bold text-transparent bg-gradient-to-r from-indigo-500 to-orange-500 bg-clip-text`}>{name}</h2>
+        <div className='flex items-center space-x-4'>
+          {vibes && vibes.map((vibe, index) => (
+            <div
+              key={`${vibe.name}-${index}`}
+              className={`pb-1 pt-1 pl-2 pr-2 rounded-full bg-gradient-to-r from-yellow-300 to-yellow-400 text-black text-sm`}
+            >
+              {vibe.name}
+            </div>
+          ))}
+        </div>
+        <div className='w-full border-b border-gray-700' />
+        <div className='flex items-center justify-between'>
+          <p className=''>👍 <span className='ml-2'>{likes}</span></p>
+          <div className=' flex space-x-4'>
+            <button className='opacity-70 hover:opacity-100'> ✏️ Edit</button>
+            <div className='h-6 border-r-2 border-slate-700' />
+            <button className='opacity-70 hover:opacity-100'> 🗑️ Delete</button>
           </div>
-        ))}
-      </div>
+        </div>
 
-    </div>
-  )
+      </div>
+    )
+  } else {
+    return (
+      <div className='bg-slate-800/20 w-full rounded-sm border-dashed border-2 border-slate-700 flex flex-col justify-center items-center hover:bg-slate-800/40 cursor-pointer '>
+        <p className='text-2xl'>🔨 Create a Mood</p>
+      </div>
+    )
+  }
+
 }
 
-export default MoodCard;
+export default memo(MoodCard);
---
+++ moloko/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+
+import Layout from '../../components/Layout';
+import MoodCard from '../../components/MoodCard';
+import { getDashboard } from "./api/getDashboard";
+
+
+const SectionHeader = ({children}) => (
+  <div>
+    <h2 className='text-white font-bold text-2xl'>{children}</h2>
+  </div>
+);
+
+const Dashboard = () => {
+  const [moodz, setMoodz] = useState(undefined);
+
+  useEffect(() => {
+    getDashboard().then((res) => setMoodz(res.data?.moodz))
+  }, [])
+
+  return (
+    <Layout>
+      <SectionHeader>Your Moodz</SectionHeader>
+      <div className='grid w-full grid-cols-4 gap-8'>
+        {moodz && moodz.map((mood, index) => (
+          <div className='w-full h-full' key={`${mood.name}-${index}`}>
+            <MoodCard
+              name={mood.name}
+              vibes={mood.vibes}
+              likes={mood.likes}
+              colors={['indigo-500', 'yellow-500']}
+            />
+          </div>
+        ))}
+        <MoodCard
+          create
+        />
+
+      </div>
+    </Layout>
+  );
+};
+
+export default Dashboard;
